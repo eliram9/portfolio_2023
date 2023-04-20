9006656355b818a846a06843dc6e0eac133681b4
eliram9 <eliram9@gmail.com>
2023-04-20 12:45:49 -0400

    navbar structure done

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,8 +8,11 @@ const Logo = () => {
   return (
     <div className='flex items-center justify-between mt-2 text-2xl font-medium'>
         <MotionLink href="/" className='w-16 h-16 bg-dark text-gold flex items-center justify-center rounded-full'
-            whileHover={{ scale: 1.1, color: "#3d3f3a", backgroundColor: "#d29829", fontSize: "25px", fontWeight: 700,
-                          transition: { duration: 1, repeat: Infinity },
+        initial={{
+            rotate: 0,
+          }}
+            whileHover={{ scale: 1.1, color: "#3d3f3a", backgroundColor: "#d29829",fontWeight: 700, rotate: [360, 0 ,180, 90, 180, 360],
+                          transition: { duration: 2, repeat: Infinity },
                           // animate: [{ opacity: 1, scale: 1 }, ]
                           // find a better effect in the future!
             }}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,81 @@
-import React from 'react';
-import Link from 'next/link';
-import Logo from './Logo';
+import React from "react";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+
+import Logo from "./Logo";
+import { GithubIcon, LinkedinIcon } from "./Icons";
+// import { AWSIcon, GithubIcon, LinkedinIcon, CodepenIcon } from "./Icons";
+
+
+const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
+    console.log(router);
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span className={`absolute h-[1px] inline-block bg-dark left-0 -bottom-0.5
+                            group-hover:w-full transition-[width] ease duration-300"
+                            ${router.asPath === href ? "w-full" : "w-0"}`}
+            >
+                &nbsp;
+            </span>
+        </Link>
+    );
+}
 
 const NavBar = () => {
-    return (
-        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+    return (  
+        <header className="w-full text-dark px-32 py-5  flex items-center justify-between">
+
             {/* Links */}
-            <nav>
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/articles">Articles</Link>
-            </nav>
-           
-           {/* Icons */}
-            <nav>
-                <Link href="/" target={"_blank"}>ICON</Link>
-                <Link href="/" target={"_blank"}>ICON</Link>
+           <nav>
+                <CustomLink href="/" title="Home" className="mr-4" />
+                <CustomLink href="/about" title="About" className="mx-4" />
+                <CustomLink href="/projects" title="Projects" className="mx-4" />
+                <CustomLink href="/articles" title="Articles" className="ml-4"/>
             </nav>
 
-            {/* Logo */}
-            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            {/* Icons */}
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="/" target={"_blank"}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 mr-3"
+
+                >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 mx-3"
+                >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 mx-3"
+                >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                    whileHover={{ y: -2 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="w-6 ml-3"
+                >
+                    <GithubIcon />
+                </motion.a>
+            </nav>
+
+            {/* Initials Logo */}
+            <div className="absolute top-2 translate-x-[-50%] left-[50%]">
                 <Logo />
             </div>
         </header>
-    )
+    );
 }
-
-export default NavBar; 
+ 
+export default NavBar;
